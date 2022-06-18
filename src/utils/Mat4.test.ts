b385ef98c7c8e2860f90c033dd2b04e45630b4bd
rodrigo.estef <rodrigoestef@gmail.com>
2022-06-17 22:45:53 -0300

    adc matriz perspective and fix multipli matriz

--- src/utils/Mat4.test.ts
+++ src/utils/Mat4.test.ts
@@ -8,5 +8,39 @@ describe("it will test mat4 class", () => {
     for (const u of unit) {
       expect(u).toBe(2);
     }
+    const sequencial = Array.from({ length: 16 }, (_, i) => i + 1);
+
+    Mat4.multiMatriz(unit, sequencial, sequencial);
+
+    const result = [
+      90, 100, 110, 120, 202, 228, 254, 280, 314, 356, 398, 440, 426, 484, 542,
+      600,
+    ];
+
+    for (let index = 0; index < result.length; index++) {
+      const element1 = result[index];
+      const element2 = unit[index];
+      expect(element1).toEqual(element2);
+    }
+  });
+
+  it("must create perspective matriz", () => {
+    const vector = Mat4.getUnitMatriz();
+    const result = Mat4.getUnitMatriz();
+    vector[0] = 1;
+    vector[1] = 1;
+    vector[2] = -100;
+    vector[3] = 1;
+    const perspective = Mat4.createPerpective(90, 1, 0.01, 100);
+    Mat4.multiMatriz(result, vector, perspective);
+    let w = result[3];
+    result.forEach((_, i) => (result[i] = result[i] / w));
+    expect(Math.abs(result[2] - 1) < 0.1).toBe(true);
+
+    vector[2] = -0.01;
+    Mat4.multiMatriz(result, vector, perspective);
+    w = result[3];
+    result.forEach((_, i) => (result[i] = result[i] / w));
+    expect(Math.abs(result[2] + 1) < 0.1).toBe(true);
   });
 });
--- src/utils/Mat4.ts
+++ src/utils/Mat4.ts
@@ -3,6 +3,45 @@ export class Mat4 {
     return [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
   }
 
+  public static createPerpective(
+    angle: number,
+    wperh: number,
+    near: number,
+    far: number
+  ) {
+    // w = -z
+    // x1 = wi/he*x0*f/w
+    // y1 = y0*f/w
+    // z1 = a/w + b
+    // -1 = a/near + b | a = -near*(1+b)
+    // 1  = a/far + b
+    // -near = a + near*b
+    // far  = a + far*b
+    // near + far = b*(far -near) | b = (near+far)/(far-near)
+
+    // z*w  = x*z0 +y
+    // z = -x + y/w
+
+    const b = (near + far) / (far - near);
+    const a = -near * (1 + b);
+
+    const cotan = 1 / Math.tan((Math.PI * angle * 0.5) / 180);
+    const perspective = Mat4.getUnitMatriz();
+
+    // cotan*wperh, 0    , 0,  0
+    // 0          , cotan, 0,  0
+    // 0          , 0    , -b, -1
+    // 0          , 0    , a,  0
+
+    perspective[0] = cotan * wperh;
+    perspective[5] = cotan;
+    perspective[10] = -b;
+    perspective[11] = -1;
+    perspective[14] = a;
+    perspective[15] = 0;
+    return perspective;
+  }
+
   public static multiMatriz(
     result: number[],
     matriz1: number[],
@@ -14,10 +53,10 @@ export class Mat4 {
     for (let j = 0; j < 4; j++) {
       for (let i = 0; i < 4; i++) {
         result[x] =
-          a[i * 4] * b[j * 4] +
-          a[i * 4 + 1] * b[j * 4 + 1] +
-          a[i * 4 + 2] * b[j * 4 + 2] +
-          a[i * 4 + 3] * b[j * 4 + 3];
+          a[j * 4] * b[4 * 0 + i] +
+          a[j * 4 + 1] * b[4 * 1 + i] +
+          a[j * 4 + 2] * b[4 * 2 + i] +
+          a[j * 4 + 3] * b[4 * 3 + i];
         x++;
       }
     }
